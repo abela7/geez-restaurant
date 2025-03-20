389f9962e512b1af68e375a5e824351b1b892c50
Abel <113767202+abela7@users.noreply.github.com>
2025-03-20 02:45:16 +0000

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -4,8 +4,8 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 // Replace these with your new Supabase project credentials
-const SUPABASE_URL = https://afpyubmeoylbrsynbfox.supabase.co;
-const SUPABASE_PUBLISHABLE_KEY =eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmcHl1Ym1lb3lsYnJzeW5iZm94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI0MzY1ODcsImV4cCI6MjA1ODAxMjU4N30.UXB4QcikkubntJUkDk-DCEtj2-w6CuLw6U3v79FR9xI;
+const SUPABASE_URL = "https://afpyubmeoylbrsynbfox.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmcHl1Ym1lb3lsYnJzeW5iZm94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI0MzY1ODcsImV4cCI6MjA1ODAxMjU4N30.UXB4QcikkubntJUkDk-DCEtj2-w6CuLw6U3v79FR9xI";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
